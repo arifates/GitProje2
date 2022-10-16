3a48998438ccff9f5c2008fc5df9527bd9091ff4
arifates <arifates44@gmail.com>
2022-10-16 23:07:22 +0300

    ilk taskımı bitirdim. Taskın adı yazıldı

--- src/Gun01/comment.java
+++ src/Gun01/comment.java
@@ -2,6 +2,13 @@ package Gun01;
 
 public class comment {
     public static void main(String[] args) {
-        
+        System.out.println("Merhaba " );
+
+        for (int i = 0; i <5 ; i++){
+            System.out.println("Merhaba " +i);
+        }
+
+
+
     }
 }
